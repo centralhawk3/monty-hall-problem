d69cb00976ecd6086263042f9594b64d8edf11d9
Glyn Jones <centralhawk3@gmail.com>
2020-05-10 18:44:04 +0100

    Allow the game to be replayed

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,30 +1,59 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Button } from '@material-ui/core';
 
 import GameCard from './GameCard';
 
 class Board extends React.Component {
-	onClick(card) {
-		console.log('Clicked!', card);
-		if (card === 'AH') {
-			console.log('You Win!');
-		}
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: uuidv4(),
+			winnerFound: false,
+			cards: this.getCards(),
+		};
+		this.handleOnClick = this.handleOnClick.bind(this);
+		this.reset = this.reset.bind(this);
 	}
 
-	render() {
-		const cardOptions = ['1J', '1J', '1J', '1J', 'AH'];
+	getCards() {
+		const cardOptions = ['1J', '1J', 'AH'];
 		const cards = [];
 		while (cardOptions.length > 0) {
 			const randomNumber = Math.floor(Math.random() * cardOptions.length);
 			cards.push(cardOptions[randomNumber]);
 			cardOptions.splice(randomNumber, 1);
 		}
+
+		return cards;
+	}
+
+	handleOnClick(card) {
+		if (card === 'AH') {
+			this.setState({winnerFound: true});
+		}
+	}
+
+	reset() {
+		this.setState({
+			id: uuidv4(),
+			cards: this.getCards(),
+			winnerFound: false,
+		});
+	}
+
+	render() {
 		return (
 			<div>
-				<h1 className="gameTitle">Find The Ace</h1>
+				<h1 className="gameTitle">{this.state.winnerFound ? 'You Win!' : 'Find The Ace'}</h1>
 			    <div className="gameSpace">
-			    	{cards.map((value, index) => {
-			        	return <GameCard card={value} onClick={this.onClick} />
+			    	{this.state.cards.map((value, index) => {
+			        	return <GameCard key={this.state.id + index + value} card={value} onClick={this.handleOnClick} />
 			     	})}
+			    </div>
+			    <div className="playAgainButton">
+			    	<Button variant="contained" color="primary" onClick={() => this.reset()}>Play Again</Button>
 			    </div>
 			</div>
 		);
