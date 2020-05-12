b74b1d0c9afa86f4f23ec733fbfe3479d726819a
Glyn Jones <centralhawk3@gmail.com>
2020-05-12 16:48:54 +0100

    Allow the cards to be revealed after making a choice. It also allows the AI to choose whether to flip a card or not

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -13,18 +13,71 @@ class Board extends React.Component {
 			id: uuidv4(),
 			winnerFound: false,
 			cards: this.getCards(),
+			cardHasBeenChosen: false,
+			message: 'Find The Ace',
 		};
 		this.handleOnClick = this.handleOnClick.bind(this);
 		this.reset = this.reset.bind(this);
 	}
 
 	getCards() {
-		return shuffleArray(['1J', '1J', 'AH']);
+		return shuffleArray([
+			{
+				face: '1J',
+				flipped: false,
+				chosen: false,
+			},
+			{
+				face: '1J',
+				flipped: false,
+				chosen: false,
+			},
+			{
+				face: 'AH',
+				flipped: false,
+				chosen: false,
+			},
+		]);
 	}
 
 	handleOnClick(card) {
-		if (card === 'AH') {
-			this.setState({winnerFound: true});
+		if (this.state.cardHasBeenChosen === false) {
+			const cardToUpdate = this.state.cards.findIndex((c) => c === card);
+			this.state.cards[cardToUpdate] = {
+				...this.state.cards[cardToUpdate],
+				chosen: !card.chosen,
+			}
+
+			this.setState({
+				cards: this.state.cards,
+				cardHasBeenChosen: true,
+				message: 'The host is deciding their next move...',
+			}, () => {
+				const shouldFlipACard = shuffleArray([0,1])[0] === 1;
+
+				if (shouldFlipACard) {
+					const cardToUpdate = this.state.cards.findIndex((c) => c.face === '1J' && c.chosen === false && c.flipped == false);
+					this.state.cards[cardToUpdate] = {
+						...this.state.cards[cardToUpdate],
+						flipped: true,
+					}
+				}
+
+				this.setState({
+					cards: this.state.cards,
+					message: shouldFlipACard
+						? 'The host has decided to flip a card for you...'
+						: 'The host has decided NOT to flip a card for you...',
+				}, () => {
+					if (shouldFlipACard) {
+						setTimeout(() => {
+							this.setState({
+								message: 'Switch your initial choice?',
+							})
+						}, 2000)
+					}					
+				});
+			});
 		}
 	}
 
@@ -33,20 +86,65 @@ class Board extends React.Component {
 			id: uuidv4(),
 			cards: this.getCards(),
 			winnerFound: false,
+			cardHasBeenChosen: false,
+			message: 'Find The Ace',
+		});
+	}
+
+	switchCardChoice() {
+		const existingChoice = this.state.cards.findIndex((c) => c.chosen === true);
+		const otherChoice = this.state.cards.findIndex((c) => c.flipped === false && c.chosen === false);
+		this.state.cards[existingChoice] = {
+			...this.state.cards[existingChoice],
+			chosen: false,
+		}
+		this.state.cards[otherChoice] = {
+			...this.state.cards[otherChoice],
+			chosen: true,
+		}
+		this.setState({
+			cards: this.state.cards,
+		});
+	}
+
+	reveal() {
+		const isChoiceAWinner = this.state.cards.findIndex((c) => c.face === 'AH' && c.chosen === true) > -1;
+
+		console.log(isChoiceAWinner, this.state.cards);
+
+		const flippedCards = this.state.cards.map((card) => {
+			return {
+				...card,
+				flipped: true,
+				chosen: false,
+			}
+		});
+
+		this.setState({
+			cards: flippedCards,
+			message: isChoiceAWinner ? 'You Win!' : 'You Lose!',
 		});
 	}
 
 	render() {
 		return (
 			<div>
-				<h1 className="gameTitle">{this.state.winnerFound ? 'You Win!' : 'Find The Ace'}</h1>
+				<h1 className="gameTitle">{this.state.message}</h1>
 			    <div className="gameSpace">
 			    	{this.state.cards.map((value, index) => {
-			        	return <GameCard key={this.state.id + index + value} card={value} onClick={this.handleOnClick} />
+			        	return <GameCard key={this.state.id + index + value.face + (value.chosen ? '1' : '0')} card={value} onClick={this.handleOnClick} />
 			     	})}
 			    </div>
-			    <div className="playAgainButton">
-			    	<Button variant="contained" color="primary" onClick={() => this.reset()}>Play Again</Button>
+			    <div className="playingButtonsGroup">
+				    <div className="playAgainButton">
+				    	<Button variant="contained" color="primary" onClick={() => this.reset()}>Play Again</Button>
+				    </div>
+				    <div className="playAgainButton">
+				    	<Button variant="contained" color="secondary" onClick={() => this.switchCardChoice()}>Switch Choice</Button>
+				    </div>
+				    <div className="playAgainButton">
+				    	<Button variant="contained" color="primary" onClick={() => this.reveal()}>Reveal</Button>
+				    </div>
 			    </div>
 			</div>
 		);
--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -3,32 +3,27 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames'; 
 
 import { Card } from '@material-ui/core';
+import CheckCircleOutlineRoundedIcon from '@material-ui/icons/CheckCircleOutlineRounded';
 
 class GameCard extends React.Component {
 
 	constructor(props) {
 		super(props);
+
 		this.state = {
-			cardClasses: 'playingCard',
-			cardIsFlipped: false,
+			markCardWrapperClasses: props.card.chosen ? 'markCardWrapper' : 'markCardWrapper hide',
 		};
-	}
-
-	flip() {
-		this.setState({
-			cardClasses: this.state.cardIsFlipped ? 'playingCard' : 'playingCard isFlipped',
-			cardIsFlipped: !this.state.cardIsFlipped,
-		});
-
-		this.props.onClick(this.props.card);
 	}
 
 	render() {
 		return (
-			<div className="playingCardScene" onClick={() => this.flip()}>
-				<div className={this.state.cardClasses}>
+			<div className="playingCardScene" onClick={() => this.props.onClick(this.props.card)}>
+				<div className={this.props.card.flipped ? 'playingCard isFlipped' : 'playingCard'}>
 					<Card className="playingCardFace card2B" />
-					<Card className="1j" className={`playingCardFace playingCardFace--front card${this.props.card}`} />
+					<div className={this.state.markCardWrapperClasses}>
+						<CheckCircleOutlineRoundedIcon className="markCard" />
+					</div>
+					<Card className="1j" className={`playingCardFace playingCardFace--front card${this.props.card.face}`} />
 				</div>
 			</div>
 		);
@@ -36,7 +31,7 @@ class GameCard extends React.Component {
 }
 
 GameCard.propTypes = {
-	card: PropTypes.string,
+	card: PropTypes.object,
 	onClick: PropTypes.func,
 };
 
