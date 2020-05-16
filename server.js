62f9bc2e2759cce0d74c72677fa844b0fa0069e6
Glyn Jones <centralhawk3@gmail.com>
2020-05-16 17:34:15 +0100

    Make host dynamic within webpack server config

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const webpackDevServer = require('webpack-dev-server');
 const webpack = require('webpack');
 
 const config = require('./webpack.config.js');
+
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || '0.0.0.0';
+
 const options = {
   contentBase: './dist',
   hot: true,
-  host: 'localhost',
+  host,
 };
 
 webpackDevServer.addDevServerEntrypoints(config, options);
 const compiler = webpack(config);
 const server = new webpackDevServer(compiler, options);
-const port = process.env.PORT || 5000;
-const host = process.env.HOST || '0.0.0.0';
 
 server.listen(port, host, () => {
   console.log(`dev server listening on port ${port}`);
